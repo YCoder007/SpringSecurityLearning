14d169a85d656ae81e2bc1d33890c95bca84b797
yashpc <yashasvib03@gmail.com>
2025-09-08 22:28:01 +0530

    Stateless for session

--- src/main/java/com/yash/projects/Spring/security/config/SecurityConfig.java
+++ src/main/java/com/yash/projects/Spring/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,8 +21,9 @@ public class SecurityConfig {
 
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
+//        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(sessions->sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
 
