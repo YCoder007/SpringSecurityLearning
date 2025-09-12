f556d9b24e2b7297e7b484931eb9a734e5201f0d
yashpc <yashasvib03@gmail.com>
2025-09-12 12:58:59 +0530

    using username and password for multiple from config classes.

--- src/main/java/com/yash/projects/Spring/security/config/SecurityConfig.java
+++ src/main/java/com/yash/projects/Spring/security/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -33,6 +37,16 @@ public class SecurityConfig {
 
 
 
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1= User.withDefaultPasswordEncoder().username("Ojas").password("o123").roles("USER").build();
+
+        UserDetails user2= User.withDefaultPasswordEncoder().username("WOW").password("w123").roles("Admin").build();
+
+        return  new InMemoryUserDetailsManager(user1, user2);
     }
 
 }
